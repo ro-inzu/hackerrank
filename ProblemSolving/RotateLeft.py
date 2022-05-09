50ac90651695bfad5a0c1d936366cfbd59b6f957
Ro <rosendoinzunza@gmail.com>
2022-05-09 15:28:07 -0700

    new problem

---
+++ ProblemSolving/RotateLeft.py
@@ -0,0 +1,18 @@
+# Rotate the array to the left d times]
+#
+
+def rotateLeft(d: int, arr: list) -> list:
+    if len(arr) < 1 or d < 1:
+        return arr
+    
+
+    new_arr = arr[d:] + arr[:d]
+    return new_arr
+
+
+d = 4
+arr = [1,2,3,4,5]
+print(rotateLeft(d,arr))
+
+# 4 rotations
+#[5, 1, 2, 3, 4]
